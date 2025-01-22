41b15ea404e85bfbe976ce952f0e5282427a8653
zahid alfahmi <158223635+zahidaof@users.noreply.github.com>
2025-01-23 00:20:28 +0300

    data base made

--- driving_analysis/api/admin.py
+++ driving_analysis/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Car, Company, DrivingData, Driver
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Car)
+admin.site.register(Company)
+admin.site.register(DrivingData)
+admin.site.register(Driver)
