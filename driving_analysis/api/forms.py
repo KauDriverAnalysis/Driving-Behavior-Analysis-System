7d4c935974a514aea18af9e1e1a1c1d94db431df
AbdullahAljehani <aboode154j@gmail.com>
2025-02-18 14:18:00 +0300

    check format for email & phonenumber

--- driving_analysis/api/forms.py
+++ driving_analysis/api/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from .models import Customer, Company, Car, Driver,DrivingData
 from django.contrib.auth.hashers import make_password
+import re
+from django.core.exceptions import ValidationError
+
 
 
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['Name', 'gender', 'phone_number', 'address', 'Password']
+    def clean_contact_number(self):
+        contact_number = self.cleaned_data.get('phone_number')
+        if not re.match(r'^(?:\+966|05)\d{8}$', contact_number):
+            raise ValidationError('Invalid phone number format for Saudi Arabia')
+        return contact_number
+    
     def save(self, commit=True):
         customer = super(CustomerForm, self).save(commit=False)
         customer.Password = make_password(self.cleaned_data['Password'])
@@ -14,16 +23,31 @@ class CustomerForm(forms.ModelForm):
             customer.save()
         return customer
 
+
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
         fields = ['Company_name', 'Contact_number', 'Email', 'location', 'Password']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('Email')
+        if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+            raise ValidationError('Invalid email format')
+        return email
+    
+    def clean_contact_number(self):
+        contact_number = self.cleaned_data.get('Contact_number')
+        if not re.match(r'^(?:\+966|05)\d{8}$', contact_number):
+            raise ValidationError('Invalid phone number format for Saudi Arabia')
+        return contact_number
+    
     def save(self, commit=True):
         company = super(CompanyForm, self).save(commit=False)
         company.Password = make_password(self.cleaned_data['Password'])
         if commit:
             company.save()
         return company
+
 
 class CarForm(forms.ModelForm):
     class Meta:
