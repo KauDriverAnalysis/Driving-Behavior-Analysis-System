b58d44caecec88ae4aff0bf642b36aa280288785
AbdullahAljehani <aboode154j@gmail.com>
2025-03-06 17:57:40 +0300

    Revert ","
    This reverts commit 8c5991b0e6864c977f14e5e55a1db5b50ff8f25b.

--- material-kit-react-main/src/paths.ts
+++ material-kit-react-main/src/paths.ts
@@ -1,26 +1,21 @@
 export const paths = {
   home: '/',
-  auth: {
-    signIn: '/auth/sign-in',
-    signUp: '/auth/sign-up',
-    resetPassword: '/auth/reset-password',
+  auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
+  dashboardAdmin: {
+    overview: '/dashboard-admin',
+    account: '/dashboard-admin/account',
+    tracking: '/dashboard-admin/tracking',
+    employees: '/dashboard-admin/employees',
+    drivers: '/dashboard-admin/drivers',
+    cars: '/dashboard-admin/cars',
+    settings: '/dashboard-admin/settings',
+    // other admin paths
   },
   dashboardCustomer: {
     overview: '/dashboard-customer',
     page1: '/dashboard-customer/page1',
-    parentalControl: '/dashboard-customer/parental-control', // Match the path name
+    ParentalControl: '/dashboard-customer/Parental-Control',
     carCustomers: '/dashboard-customer/car-customers',
   },
-  dashboardAdmin: {
-    overview: '/dashboard-admin',
-    tracking: '/dashboard-admin/tracking',
-    employees: '/dashboard-admin/employees',
-    drivers: '/dashboard-admin/drivers',
-    settings: '/dashboard-admin/settings',
-    account: '/dashboard-admin/account',
-    cars: '/dashboard-admin/cars',
-  },
-  errors: { 
-    notFound: '/errors/not-found' 
-  },
+  errors: { notFound: '/errors/not-found' },
 } as const;
