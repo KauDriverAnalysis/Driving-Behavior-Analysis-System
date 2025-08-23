493a5e9ce5d5d3e2168844db65d8070aacafbf7b
AbdullahAljehani <aboode154j@gmail.com>
2025-08-23 23:17:06 +0300

    Revert "ز"
    This reverts commit 8518530084b79ed886d6c4f6896bdac894f39841.

--- material-kit-react-main/src/app/dashboard/admin/accident-analysis/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import * as React from 'react';
-import { AccidentAnalysisDashboard } from '@/components/dashboard-admin/Simulation/AccidentAnalysisDashboard';
-
-export default function AccidentAnalysisPage() {
-  return <AccidentAnalysisDashboard />;
-}
